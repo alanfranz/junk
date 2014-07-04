7eeec5f19dc26c529ec6eaa16985f9f9c609ef45
bls <blair.strang@gmail.com>
2014-07-03 21:46:25 -0700

    Make mini_ipgen.py.

---
+++ mini_ipgen.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# If you just want a few random IPs, this might be easier...
+#
+
+import netaddr, random, os, sys
+
+def random_ips(num, bogons_file=None):
+    # Load bogons
+    bogon_networks = []
+    if bogons_file is not None:
+        with open('bogons.txt', 'r') as fp:
+            for line in fp:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                net = netaddr.IPNetwork(line, implicit_prefix=False, version=4)
+                bogon_networks.append(net)
+    bogon_ipset = netaddr.IPSet(bogon_networks)
+    print bogon_ipset
+
+    # Generate IPs
+    ips = set()
+    while len(ips) < num:
+        print len(ips)
+        x = random.randint(0, 0xFFFFFFFF)
+        ip = netaddr.IPAddress(x)
+        if ip not in bogon_ipset:
+            ips.add(ip)
+
+    return ips
+
+def main():
+    if len(sys.argv) < 2:
+        print "Usage: mini_ipgen.py <number-of-ips>"
+        sys.exit(1)
+
+    num = int(sys.argv[1])
+    for ip in random_ips(num, 'bogons.txt'):
+        print ip
+
+if __name__ == '__main__':
+    main()
+    
